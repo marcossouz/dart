cda08068006a9f9011d142cdd789add5e91b0d69
Marcos Souza <marcos@fontedeprecos.com.br>
2020-12-05 10:26:50 -0300

    api aqueduct

---
+++ heroes/lib/channel.dart
@@ -0,0 +1,26 @@
+import 'controller/heroes_controller.dart';
+import 'heroes.dart';
+
+class HeroesChannel extends ApplicationChannel {
+  @override
+  Future prepare() async {
+    logger.onRecord.listen((rec) => print("$rec ${rec.error ?? ""} ${rec.stackTrace ?? ""}"));
+  }
+
+  @override
+  Controller get entryPoint {
+    final router = Router();
+
+    router
+      .route('/heroes/[:id]')
+      .link(() => HeroesController());
+
+    router
+      .route("/example")
+      .linkFunction((request) async {
+        return Response.ok({"key": "value"});
+      });
+
+    return router;
+  }
+}
---
+++ heroes/lib/controller/heroes_controller.dart
@@ -0,0 +1,27 @@
+import 'package:aqueduct/aqueduct.dart';
+import 'package:heroes/heroes.dart';
+
+class HeroesController extends ResourceController {
+  final _heroes = [
+    {'id': 11, 'name': 'Mr. Nice'},
+    {'id': 12, 'name': 'Narco'},
+    {'id': 13, 'name': 'Bombasto'},
+    {'id': 14, 'name': 'Celeritas'},
+    {'id': 15, 'name': 'Magneta'},
+  ];
+
+  @Operation.get()
+  Future<Response> getAllHeroes() async {
+    return Response.ok(_heroes);
+  }
+
+  @Operation.get('id')
+  Future<Response> getHeroByID() async {
+    final id = int.parse(request.path.variables['id']);
+    final hero = _heroes.firstWhere((hero) => hero['id'] == id, orElse: () => null);
+    if (hero == null) {
+      return Response.notFound();
+    }
+    return Response.ok(hero);
+  }
+}
